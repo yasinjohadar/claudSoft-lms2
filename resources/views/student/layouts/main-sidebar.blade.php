3ada1345af31b5578ad7dfeeaf30d30104f1956c
yasin <yassinjokhdar@gmail.com>
2026-01-11 11:59:15 +0300

    delete courses from student sidebar

--- resources/views/student/layouts/main-sidebar.blade.php
+++ resources/views/student/layouts/main-sidebar.blade.php
@@ -75,18 +75,6 @@
                         </li>
                         <!-- End::slide -->
 
-                        <!-- Start::slide - تصفح الكورسات -->
-                        <li class="slide">
-                            <a href="{{ route('student.courses.index') }}" class="side-menu__item">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                    <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
-                                    <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
-                                </svg>
-                                <span class="side-menu__label">تصفح الكورسات</span>
-                            </a>
-                        </li>
-                        <!-- End::slide -->
-
                         <!-- Start::slide - كورساتي -->
                         <li class="slide">
                             <a href="{{ route('student.courses.my-courses') }}" class="side-menu__item">
